e44cbae24336a948b67a545d566f2b6b890ab44f
Dian Wulandari <dianw185@gmail.com>
2026-09-15 09:38:37 +0800

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -8,7 +8,7 @@
    policy di database, bukan dengan menyembunyikan key ini.
    ========================================================================= */
 
-const SUPABASE_URL = "https://peyyootevwqydlwfxzic.supabase.co/rest/v1/"; // contoh: https://xxxxx.supabase.co
+const SUPABASE_URL = "https://peyyootevwqydlwfxzic.supabase.co"; // contoh: https://xxxxx.supabase.co
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBleXlvb3RldndxeWRsd2Z4emljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkzMzQzMDksImV4cCI6MjEwNDkxMDMwOX0.YZD7iX3oqYpH6Hnki9_z1U6_7Bh7rnjfWx93uexuGEs";
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
